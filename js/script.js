5de070ed7a7d4009fe98914c341d792850067857
carolina <carorua19@gmail.com>
2022-03-29 13:45:24 -0500

    resubo proyecto

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function cambia_de_pagina(ciudad){
+
+      if(ciudad==="bogota"){
+            location.href="bogota.html"
+      }
+      else if(ciudad==="medellin"){
+            location.href="medellin.html"
+      }
+         
+}
+
+//
+
+
